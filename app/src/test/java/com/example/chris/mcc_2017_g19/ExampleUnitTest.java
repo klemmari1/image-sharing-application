cda49cb45f780821518b21aaf6ff6a5abf1235e8
Klemets Kristian <kristian.klemets@gmail.com>
2017-11-03 14:34:23 +0200

    Initial commit

---
+++ app/src/test/java/com/example/chris/mcc_2017_g19/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.chris.mcc_2017_g19;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
