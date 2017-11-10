9c0873c50be0b60ab8937fba7196137b248d8097
Alessio Spallino <alessio.spallino@aalto.fi>
2017-11-10 15:19:52 +0200

    fix

--- app/src/main/java/com/example/chris/mcc_2017_g19/CustomAdapter.java
+++ app/src/main/java/com/example/chris/mcc_2017_g19/CustomAdapter.java
@@ -1,13 +1,71 @@
 package com.example.chris.mcc_2017_g19;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class CustomAdapter extends AppCompatActivity {
+import java.util.List;
+
+public class CustomAdapter extends BaseAdapter {
+
+    private LayoutInflater layoutinflater;
+    private List<ItemObject> listStorage;
+    private Context context;
+
+    public CustomAdapter(Context context, List<ItemObject> customizedListView) {
+        this.context = context;
+        layoutinflater =(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        listStorage = customizedListView;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom_adapter);
+    public int getCount() {
+        return listStorage.size();
     }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder listViewHolder;
+        if(convertView == null){
+            listViewHolder = new ViewHolder();
+            convertView = layoutinflater.inflate(R.layout.activity_custom_adapter, parent, false);
+            listViewHolder.textInListView = (TextView)convertView.findViewById(R.id.title);
+            listViewHolder.imageInListView = (ImageView)convertView.findViewById(R.id.imageView);
+            listViewHolder.imageCloudInListView = (ImageView)convertView.findViewById(R.id.imagecloud);
+            convertView.setTag(listViewHolder);
+        }else{
+            listViewHolder = (ViewHolder)convertView.getTag();
+        }
+
+        /* populate the basic info of each album (image,title,cloud or not)*/
+        listViewHolder.textInListView.setText(listStorage.get(position).getTitle());
+        int imageResourceId = this.context.getResources().getIdentifier(listStorage.get(position).getImageResource(), "drawable", this.context.getPackageName());
+        listViewHolder.imageInListView.setImageResource(imageResourceId);
+        int imageCloudId = this.context.getResources().getIdentifier(listStorage.get(position).getCloud(), "drawable", this.context.getPackageName());
+        listViewHolder.imageCloudInListView.setImageResource(imageCloudId);
+
+        return convertView;
+    }
+
+    static class ViewHolder{
+        TextView textInListView;
+        ImageView imageInListView;
+        ImageView imageCloudInListView;
+    }
+
 }
--- app/src/main/java/com/example/chris/mcc_2017_g19/ItemObject.java
+++ app/src/main/java/com/example/chris/mcc_2017_g19/ItemObject.java
@@ -5,4 +5,38 @@ package com.example.chris.mcc_2017_g19;
  */
 
 public class ItemObject {
+
+    private String title;
+    private String imageResource;
+    private String cloud;
+
+    public ItemObject(String title, String imageResource, String cloud) {
+        this.title = title;
+        this.imageResource = imageResource;
+        this.cloud = cloud;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageResource() {
+        return imageResource;
+    }
+
+    public void setImageResource(String imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public String getCloud() {
+        return cloud;
+    }
+
+    public void setCloud(String cloud) {
+        this.cloud = cloud;
+    }
 }
