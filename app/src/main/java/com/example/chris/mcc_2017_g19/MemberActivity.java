3f69458048c24de353242b1d33104c57ddf7b491
Klemets Kristian <kristian.klemets@gmail.com>
2017-11-10 14:30:07 +0200

    Added leave button to menu bar

--- app/src/main/java/com/example/chris/mcc_2017_g19/MemberActivity.java
+++ app/src/main/java/com/example/chris/mcc_2017_g19/MemberActivity.java
@@ -3,8 +3,11 @@ package com.example.chris.mcc_2017_g19;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,11 +33,26 @@ public class MemberActivity extends AppCompatActivity {
         members.add("Mark");
         members.add("Joe");
         memberAdapter.notifyDataSetChanged();
+
+        TextView nameValue = (TextView) findViewById(R.id.group_info_name_value);
+        TextView expirationValue = (TextView) findViewById(R.id.group_info_expiration_value);
+
+        nameValue.setText("Dummy Group");
+        expirationValue.setText("Tue 31 Oct - 10:00pm");
     }
 
     public void onClick(View v) {
         Intent intent = new Intent(this, MemberQRActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.leave_button, menu);
+
+        MenuItem item = menu.findItem(R.id.action_leave);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     public List<String> getMembers() {
