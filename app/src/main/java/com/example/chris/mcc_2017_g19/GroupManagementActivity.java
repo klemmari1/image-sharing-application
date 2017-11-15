c342631d7f18c43955da6afcf06641124aa0fbeb
Tommi Saarinen <tommi.saarinen@aalto.fi>
2017-11-15 22:31:57 +0200

    Fix created group to have name provided by user

--- app/src/main/java/com/example/chris/mcc_2017_g19/GroupManagementActivity.java
+++ app/src/main/java/com/example/chris/mcc_2017_g19/GroupManagementActivity.java
@@ -29,7 +29,6 @@ public class GroupManagementActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_group);
 
         //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        //
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
@@ -50,9 +49,8 @@ public class GroupManagementActivity extends AppCompatActivity {
         createGroupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String groupName = "Test1"; //TODO Replace with value from name field
                 try {
-                    addGroup(groupName);
+                    addGroup();
                 } catch (IllegalArgumentException e){
                     Toast.makeText(GroupManagementActivity.this, "Give a proper group name", Toast.LENGTH_SHORT).show(); //TODO Placeholder toast, add meaningful notification
                     ; //TODO Toast?
@@ -63,13 +61,13 @@ public class GroupManagementActivity extends AppCompatActivity {
         //Check whether current user is in a group or not
     }
 
-    private void addGroup(String groupName) throws IllegalArgumentException {
+    private void addGroup() throws IllegalArgumentException {
         EditText nameField = (EditText) findViewById(R.id.fieldGroupName); //TODO Handle name getting outside function to bypass empty check here?
         String nameValue = nameField.getText().toString();
         if (nameValue.isEmpty())
             throw new IllegalArgumentException("No group name provided");
-        GroupObject group = new GroupObject(groupName);
-        mDatabase.child("groups").child(groupName).setValue(group); //TODO Register by GroupID?
+        GroupObject group = new GroupObject(nameValue);
+        mDatabase.child("groups").child(nameValue).setValue(group); //TODO Register by GroupID?
         Toast.makeText(GroupManagementActivity.this, "Group added to database", Toast.LENGTH_SHORT).show();
     }
 }
