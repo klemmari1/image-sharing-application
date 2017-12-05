cda72b5af6d3274720f5ea163514341a9c17fb54
Teemu Toikkanen <teemu.toikkanen@aalto.fi>
2017-12-05 16:53:44 +0200

    switching from laptop to home pc

--- app/src/main/java/com/example/chris/mcc_2017_g19/MainActivity.java
+++ app/src/main/java/com/example/chris/mcc_2017_g19/MainActivity.java
@@ -35,6 +35,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -74,6 +75,13 @@ public class MainActivity extends AppCompatActivity {
 
         DatabaseReference userReference = databaseReference.child("users").child(firebaseUser.getUid());
         userReference.keepSynced(true);
+
+        //testing notification stuff (fcm)
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        userReference.child("deviceTokens").child(token).setValue(1);
+        Log.d(TAG,"token token token:" + token);
+
 
         userReference.addValueEventListener(new ValueEventListener() {
             @Override
