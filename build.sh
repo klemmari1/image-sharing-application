ebcdc3cdda079ba24c99fa6e98f68becf3493bdb
Tommi Saarinen <tommi.saarinen@aalto.fi>
2017-12-10 11:08:55 +0200

    Add initial build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Building a debug APK ...
+./gradlew assembleDebug
+
+echo Setting gcloud project ...
+# gcloud config set project mcc-fall-2017-g19
+
+echo Deploying app ...
+# gcloud app deploy Backend/app.yaml
+
+echo Deploying cron job ...
+# gcloud app deploy Backend/cron.yaml
